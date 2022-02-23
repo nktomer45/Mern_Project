88e4cde69d9121ddf04d98aa80ea9111f4a404b4
neerajkumar123456 <neeraj.kumar@successive.tech>
2022-02-23 16:54:31 +0530

    CCHAT-APP

---
+++ client/src/Component/message/Message.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Message.css';
+
+const Message = ({user,message,classs }) => {
+if(user){
+    return (
+        <>
+        <div className={`MessageBox ${classs}`}>
+            {`${user}:${message}`}
+        </div>
+        </>
+    )
+}
+else{
+    return (
+        <>
+        <div className={`MessageBox ${classs}`}>
+            { `you: ${message}`}
+        </div>
+        </>
+    )
+}
+}
+export default Message;
